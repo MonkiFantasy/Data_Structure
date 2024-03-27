f7e9d7d5af033d5c28e9af638759d770dd79e009
monki <monki4536251@outlook.com>
2024-03-27 17:47:11 +0800

    init

---
+++ sqlist.cpp
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#define MAX_SIZE=50
+
+typedef struct{
+	ElemType data[MAX_SIZE];
+	int length;
+}sqList;
+
+void initList(sqList &L){
+}
+
+bool listInsert(){}
+
+bool listDelete(){}
+
+ElemType getElem(){}
+
+int locateElem(){}
+
+int main(){
+
+	return 0;
+}
